331928ac76a5f5ae3eacbacf687fe88de8e6b58e
artem-vt <102740097+artem-vt@users.noreply.github.com>
2022-06-29 18:48:51 +0300

    Added the second scenario

--- cypress/e2e/login.feature
+++ cypress/e2e/login.feature
@@ -4,6 +4,15 @@ Feature: Login
     When I open the "http://dvl-test-frontend.s3-website-us-east-1.amazonaws.com"
     And I input "artem.vepryk@selectoglobal.com" into the "email_address_input" field
     And I input "007007bo" into the "password_input" field
-    And I click "sign_in_button" 
+    And I click Sign In button 
     Then I see I am logged in
     And the url is "http://dvl-test-frontend.s3-website-us-east-1.amazonaws.com/accounting/reports/company"
+
+Scenario: Login with the wrong creds
+    When I open the "http://dvl-test-frontend.s3-website-us-east-1.amazonaws.com"
+    And I input "artem.vepryk@selectoglobal.com" into the "email_address_input" field
+    And I input "wrongPass" into the "password_input" field
+    And I click Sign In button 
+    Then I see I am NOT logged in
+    And I see Invalid Credentials error message
+    And the url is "http://dvl-test-frontend.s3-website-us-east-1.amazonaws.com/login"
--- cypress/e2e/login/login.js
+++ cypress/e2e/login/login.js
@@ -8,15 +8,24 @@ When(`I input {string} into the {string} field`, (input, field)=>{
   cy.get(`[data-test=${field}]`).type(input);
 })
 
-When(`I click {string}`, (buttonName)=>{
-  cy.get(`[data-test=${buttonName}`).click();
+When(`I click Sign In button`, (buttonName)=>{
+  cy.intercept('https://staging-api.logistic.expert/v1/employee/session').as('sessionData');
+  cy.get(`[data-test="sign_in_button"`).click();
+  cy.wait('@sessionData');
 })
 
 Then(`I see I am logged in`, (url)=>{
-  cy.intercept('https://staging-api.logistic.expert/v1/employee/session').as('getSession');
-  cy.wait('@getSession').its('response.statusCode').should('eq', 201);
+  cy.get('@sessionData').its('response.statusCode').should('eq', 201);
+})
+
+Then(`I see I am NOT logged in`, (url)=>{
+  cy.get('@sessionData').its('response.statusCode').should('eq', 401);
 })
 
 Then(`the url is {string}`, (url)=>{
   setTimeout(cy.url().should('eq', url),3000);
+})
+
+Then(`I see Invalid Credentials error message`, ()=>{
+  cy.get(`[data-test="error_p"`).should("exist");
 })
